b11c60616b810493e82e45f3099a077b792f7b69
onurinanc <inanc.dogruyol@metu.edu.tr>
2022-12-05 13:51:16 +0300

    complete proof

---
+++ scripts/export_verifier.sh
@@ -0,0 +1,1 @@
+snarkjs zkey export solidityverifier multiplier2_0001.zkey verifier.sol
---
+++ scripts/verify.sh
@@ -0,0 +1,3 @@
+snarkjs groth16 prove multiplier2_0001.zkey ./multiplier2_js/witness.wtns proof.json public.json
+
+snarkjs groth16 verify verification_key.json public.json proof.json
